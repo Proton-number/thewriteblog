c44a004f4712a2ace1907d5ee644d90b39ef33ef
Proton-number <favouradebimpe63@gmail.com>
2025-12-29 10:13:01 +0100

    Refactor code structure for improved readability and maintainability

--- thewriteblog/sanity.cli.ts
+++ thewriteblog/sanity.cli.ts
@@ -2,8 +2,8 @@ import {defineCliConfig} from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: 'dtbj3t1s',
-    dataset: 'production'
+    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    dataset: 'production',
   },
   deployment: {
     /**
@@ -11,5 +11,5 @@ export default defineCliConfig({
      * Learn more at https://www.sanity.io/docs/cli#auto-updates
      */
     autoUpdates: true,
-  }
+  },
 })
